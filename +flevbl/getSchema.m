f035b7cac3c78ced0808cf150cd0393b026d6dda
mani33 <manivannan33@yahoo.com>
2013-10-10 10:05:21 -0500

    Removed +flevbl class as it is replaced by +fle

--- +flevbl/getSchema.m
+++
@@ -1,6 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-        schemaObject = dj.Schema(dj.conn,'flevbl', 'mani_phys_fle_var_bar_lum');
-end
-obj = schemaObject;
